9f115c70e47ddfb7379d3ffa8a7aff5dcf3d85f5
Ivan <ivanp@cern.ch>
2017-02-23 23:04:22 +0100

    herwigpp listing added

--- Docs/listing/list_print.pl
+++ Docs/listing/list_print.pl
@@ -3,14 +3,15 @@
 open(IN,"list");
 #open(OUT,">13TeV_data_2015C_FSQJets3");
 #open(OUT,">13TeV_data_2015C_Min_Bias");
-open(OUT,">13TeV_data_2015C_ZeroBias");
+#open(OUT,">13TeV_data_2015C_ZeroBias");
+#open(OUT,">13TeV_mc_herwigpp_MinBias");
+#open(OUT,">13TeV_mc_herwigpp_pt10to35");
+#open(OUT,">>13TeV_mc_herwigpp_pt10to35_FB");
+#open(OUT,">13TeV_mc_herwigpp_pt35toInf");
+open(OUT,">13TeV_mc_herwigpp_pt35toInf_FB");
 @inn=<IN>;
 for ($i=0; $i<=$#inn; $i++){
-	#print OUT "/afs/cern.ch/work/i/ivanp/CFF_SOFT/CFF_SKIMS/13TeV/data_run2015C/$inn[$i]";
-	for ($j=1; $j<=8; $j++){
-		#print OUT "/afs/cern.ch/work/i/ivanp/CFF_SOFT/CFF_SKIMS/13TeV/data_run2015C/L1MinimumBiasHF$j/$inn[$i]";
-		print OUT "/afs/cern.ch/work/i/ivanp/CFF_SOFT/CFF_SKIMS/13TeV/data_run2015C/ZeroBias$j/$inn[$i]";
-	}
+	print OUT "/afs/cern.ch/work/i/ivanp/CFF_SOFT/CFF_SKIMS/13TeV/herwigpp_det/QCD_Pt35toInf_FB_13TeV_herwigpp_noPU/$inn[$i]";
 }
 close(IN);
 close(OUT); 
